de19be935a8d5b2f1f517d06124ff32a7fb79b46
Martin Colwell <colwem@gmail.com>
2016-04-10 18:24:47 -0400

    Total reorganization

--- readme.md
+++ readme.md
@@ -1,4 +1,108 @@
-let numberSystem = places.fromArray([365,24,60,60]);
+# Mixed-Radix
+
+### Create representations of mixed radix numeral systems
+
+A mixed radix numeral system is like our time system or imperial measurement
+systems. For instance you can measure time in weeks,days,hours,minutes,seconds.
+To represent this system so that you can do math, extract the value at specific
+places, and convert to and from regular numbers you can use this package. 
+
+### Usage
+
+```js
+const mixedRadix = require('mixed-radix');
+
+// Convert 100000 seconds to a more understandable format
+
+let seconds = 100000;
+
+let timeRadices = [24, 60, 60]
+/* this means our numberalSystem allows up to but not including 60 seconds 
+ * in the first position, 60 minutes in the second position, and 24 hours in
+ * the third position.  The fourth position, days, is unlimited.  We could
+ * do [365.25, 24, 60, 60] which would add a years position. 
+ */
+
+let time = mixedRadix(100000, timeRadices);
+/* create a mixedRadixNumber whose value is 100000 seconds and who's 
+ * numeralSystem is defined by timeRadices
+ */
+
+time.representation();
+/* [1, 3, 46, 40] 
+ * That's 1 day, 3 hours, 46 minutes and 40 seconds
+ */
+
+// Express 100,000 seconds in hours
+
+let newTime = time.shiftPoint(-2);
+/* move the radix point 2 positions to the left.  This makes the "zeros" 
+ * place the hours place.
+ */
+
+let hours = newTime.value();
+/* 
+ * 
+
+// Altogether, converting 100,000 seconds to hours
+
+let hours = mixedRadix(100000, [24, 60, 60]).shiftPoint(-2).value();
+```
+
+// Create mixed-radix number 
+
+let num = mixedRadix(40, [4,3,2]);
+
+// num will now have an internal representation of 
+// [1, 2, 2, 0] and that corresponds to 1 * 4*3*2 + 2 * 3*2 + 2 * 2 + 0
+
+// With a specific radix point
+let num = mixedRadix(40, [4,3,2], -1);
+
+// make a numeral system
+let system = mixedRadix.make([4,3,2]);
+
+// With specific radix point
+let system = mixedRadix.make([4,3,2], -1);
+
+num.representation() 
+// [1, 2, 2, 0]
+
+num.value() 
+// 40
+
+num.place(0) 
+// 0 
+
+num.place(3) 
+// 1  
+
+// move the radix point one position to the left, equivalent to multipying a base 10 number by 10^(-1). Returns new number.
+let newNum = num.shiftRadixPoint(-1);
+newNum.toNumber();
+// [value]
+
+// The ; represents the radix point (decimal point), oo represents infinity
+
+num.toString() 
+// '1, 2, 2, 0;\noo, 4, 3, 2'
+
+// using unicode subscript notation
+num.toSubscriptNotation() 
+// 1ₒₒ2₄2₃0₂
+
+// first converts number to back to base 10, performs the addition, 
+// then converts to mixed-radix number number back, performs addition, then converts
+let newNum = num.add(20);
+
+// can add mixed-radix numbers too
+let another = num.add(newNum);
+
+
+
+
+
+l et numberSystem = places.fromArray([365,24,60,60]);
 numberSystem.lowerEndBase(10);
 numberSystem.upperEndBase(10);
 numberSystem.names(['days', ''], zeroPlace)
--- test/lib/index.js
+++ test/lib/index.js
@@ -3,86 +3,31 @@
 const chai   = require('chai'),
       expect = chai.expect,
       mixedRadix = require('../../lib'),
+      assert = require('assert'),
       _ = require('lodash');
 
 
+let radices = [4,3,2];
 
-describe('exported object', function() {
+describe('exposed API', function() {
 
-  const places = [4,3,2];
+  describe('mixedRadix()', function() {
 
-  describe('#createMixedRadix', function() {
-
-    it('creates MixedRadix object with places and divs', function() {
-      const mr = mixedRadix.createMixedRadix({places});
-
-      expect(mr._places).to.eql([Infinity].concat(places));
-      expect(mr._divs).to.eql([Infinity, 24,6,2]);
+    it('creates object', function() {
+      let val = mixedRadix(4.1, radices, -1);
+      let rep = val.representation();
     });
 
   });
 
-  describe('#convert', function() {
-
-    it('converts 33 to [4,3,2] based [1,1,1,1] number', function() {
-
-      const mrn = mixedRadix.convert(33, places);
-      expect(mrn._array).to.eql([1,1,1,1]);
-
+  describe('mixedRadix.withNs()', function() {
+    let ns;
+    before(function()  {
+      ns = mixedRadix.numeralSystem(radices);
     });
 
-  });
-
-});
-
-describe('MixedRadix', function() {
-
-  const places = [4,3,2];
-
-  describe('#fromArray', function() {
-
-
-  });
-
-  describe('#convert', function() {
-
-    it('converts 33 to 1,1,1,1 in 4,3,2 base', function() {
-      const mr = mixedRadix.fromArray(places);
-
-      const result = mr.convert(33)._array
-      expect(result).to.eql([1,1,1,1]);
-    });
-
-    it('converts 0 to 0,0,0,0 in 4,3,2 base', function() {
-      const mr = mixedRadix.fromArray(places);
-
-      const result = mr.convert(0)._array
-      expect(result).to.eql([0,0,0,0]);
-    });
-
-  });
-
-  describe('#add', function() {
-
-    it('adds 10 and 23 to get 20', function() {
-      const mr = mixedRadix.fromArray(places);
-
-      const n1 = mr.convert(23);
-      const n2 = mr.convert(10);
-      const result = mr.add(n1, n2);
-      expect(result._array).to.eql([1,1,1,1]);
-    });
-
-  });
-
-  describe('#toNumber', function() {
-
-    it('converts [1,1,1,1] back to 33', function() {
-      const mr = mixedRadix.fromArray(places);
-
-      const n = mr.convert(33);
-      const result = mr.toNumber(n);
-      expect(result).to.equal(33);
+    it('creates mixedRadixNumber with ns', function() {
+      let val = mixedRadix.withNs(40, ns)
     });
 
   });
@@ -90,47 +35,271 @@ describe('MixedRadix', function() {
 });
 
 
-describe('MRNumber', function() {
-  let places, mr;
+describe('mixedRadixNumber', function() {
 
-  beforeEach(function() {
+  describe('constructor', function() {
 
-    places = [4,3,2];
-    mr = mixedRadix.fromArray(places);
-  });
+    it('creates mixed number', function() {
+      expect(mixedRadix(40, radices)).to.be.ok;
+    });
 
-  describe('#place', function() {
+    it('asserts valid radixPoint', function() {
+      expect(() => mixedRadix(40, radices, 3))
+        .to.throw(assert.AssertionError)
+      expect(() => mixedRadix(40, radices, -10))
+        .to.throw(assert.AssertionError)
+    });
 
-    it('gets the 0 place number', function() {
-      const mrn = mr.convert(1)
-      const place = mrn.place(0);
+    describe('#fromRepresentation', function() {
 
-      expect(place).to.equal(1);
+      it('creates number', function() {
+        let rep = [1, 2, 2, 0];
+        expect(mixedRadix.fromRepresentation(rep, radices)).to.be.ok;
+        rep = [1, 2, 2];
+        expect(mixedRadix.fromRepresentation(rep, radices)).to.be.ok;
+        rep = [1, 2];
+        expect(mixedRadix.fromRepresentation(rep, radices)).to.be.ok;
+      });
+
+      it('fills reps that are too short', function() {
+        let rep = [2, 1];
+        let mrn = mixedRadix.fromRepresentation(rep, radices);
+        expect(mrn.representation()).to.eql([0,0,2,1]);
+      });
+
+      it('truncates representations that are too long', function() {
+        let rep = [5, 1, 2, 2, 0];
+        let mrn = mixedRadix.fromRepresentation(rep, radices);
+
+        expect(mrn.representation()).to.eql([ 1, 2, 2, 0]);
+      });
+
     });
 
   });
 
-  describe('#toString', function() {
+  describe('methods', function() {
+    let mrn;
 
-    it('turns 33 into the string "1, 1, 1, 1;"', function() {
-      const mrn = mr.convert(33);
-      const str = mrn.toString();
-
-      expect(str).to.equal('1, 1, 1, 1;');
+    beforeEach(function() {
+      mrn = mixedRadix(40, radices);
     });
-  });
 
-  describe('#add', function() {
+    describe('#atPlace', function() {
 
-    it('adds 10 and 23 and gets 33', function() {
-      const n1 = mr.convert(10);
-      const n2 = mr.convert(23);
+      it('returns value at place', function() {
+        expect(mrn.atPlace(0)).to.equal(0);
+        expect(mrn.atPlace(1)).to.equal(2);
+        expect(mrn.atPlace(2)).to.equal(2);
+        expect(mrn.atPlace(3)).to.equal(1);
+      });
 
-      const n3 = n1.add(n2);
+      it('errors when out of bounds', function() {
+        expect(() => mrn.atPlace(-1)).to.throw(assert.AssertionError);
+        expect(() => mrn.atPlace(4)).to.throw(assert.AssertionError);
+      });
 
-      expect(n3._array).to.eql([1,1,1,1]);
     });
+
+    describe('#representation', function() {
+
+      it('returns array representation', function() {
+        expect(mrn.representation()).to.eql([1,2,2,0]);
+      });
+
+    });
+
+    describe('#shiftPoint', function() {
+
+      it('returns a new number', function() {
+        expect(mrn.shiftPoint(-2)).to.be.ok;
+      });
+
+      it('needs a value', function() {
+        expect(() => mrn.shiftPoint()).to.throw(assert.AssertionError);
+      });
+
+      it('converts seconds to hours', function() {
+        let hours = mixedRadix(60 * 60 * 3, [24, 60, 60])
+          .shiftPoint(-2).value();
+        expect(hours).to.equal(3);
+      });
+
+    });
+
+    describe('#value', function() {
+
+      it('returns the value', function() {
+        expect(mrn.value()).to.equal(40);
+      });
+
+      it('creates a value from representation', function() {
+        let mrn = mixedRadix.fromRepresentation([1,2,2,0], radices);
+        expect(mrn.value()).to.equal(40);
+      });
+    });
+
 
   });
 
 });
+
+
+
+/*
+ * describe('exported object', function() {
+ *
+ *   const places = [4,3,2];
+ *
+ *   describe('#createMixedRadix', function() {
+ *
+ *     it('creates MixedRadix object with places and divs', function() {
+ *       const mr = mixedRadix.createMixedRadix({places});
+ *
+ *       expect(mr._places).to.eql([Infinity].concat(places));
+ *       expect(mr._divs).to.eql([Infinity, 24,6,2]);
+ *     });
+ *
+ *   });
+ *
+ *   describe('#convert', function() {
+ *
+ *     it('converts 33 to [4,3,2] based [1,1,1,1] number', function() {
+ *
+ *       const mrn = mixedRadix.convert(33, places);
+ *       expect(mrn._array).to.eql([1,1,1,1]);
+ *
+ *     });
+ *
+ *   });
+ *
+ * });
+ *
+ * describe('MixedRadix', function() {
+ *
+ *   const places = [4,3,2];
+ *
+ *   describe('#fromArray', function() {
+ *
+ *
+ *   });
+ *
+ *   describe('#convert', function() {
+ *
+ *     it('converts 33 to 1,1,1,1 in 4,3,2 base', function() {
+ *       const mr = mixedRadix.fromArray(places);
+ *
+ *       const result = mr.convert(33)._array
+ *       expect(result).to.eql([1,1,1,1]);
+ *     });
+ *
+ *     it('converts 0 to 0,0,0,0 in 4,3,2 base', function() {
+ *       const mr = mixedRadix.fromArray(places);
+ *
+ *       const result = mr.convert(0)._array
+ *       expect(result).to.eql([0,0,0,0]);
+ *     });
+ *
+ *   });
+ *
+ *   describe('#add', function() {
+ *
+ *     it('adds 10 and 23 to get 20', function() {
+ *       const mr = mixedRadix.fromArray(places);
+ *
+ *       const n1 = mr.convert(23);
+ *       const n2 = mr.convert(10);
+ *       const result = mr.add(n1, n2);
+ *       expect(result._array).to.eql([1,1,1,1]);
+ *     });
+ *
+ *   });
+ *
+ *   describe('#toNumber', function() {
+ *
+ *     it('converts [1,1,1,1] back to 33', function() {
+ *       const mr = mixedRadix.fromArray(places);
+ *
+ *       const n = mr.convert(33);
+ *       const result = mr.toNumber(n);
+ *       expect(result).to.equal(33);
+ *     });
+ *
+ *   });
+ *
+ * });
+ *
+ *
+ * describe('MRNumber', function() {
+ *   let places, mr;
+ *
+ *   beforeEach(function() {
+ *
+ *     places = [4,3,2];
+ *     mr = mixedRadix.fromArray(places);
+ *   });
+ *
+ *   describe('#place', function() {
+ *
+ *     it('gets the 0 place number', function() {
+ *       const mrn = mr.convert(1)
+ *       const place = mrn.place(0);
+ *
+ *       expect(place).to.equal(1);
+ *     });
+ *
+ *     it('gets the 1 place number', function() {
+ *       const mrn = mr.convert(1)
+ *       const place = mrn.place(1);
+ *
+ *       expect(place).to.equal(0);
+ *     });
+ *
+ *   });
+ *
+ *   describe('#toArray', function() {
+ *
+ *     it('turns returns the array"', function() {
+ *       const mrn = mr.convert(33);
+ *       const array = mrn.toArray();
+ *
+ *       expect(array).to.eql([1,1,1,1]);
+ *     });
+ *
+ *   });
+ *
+ *   describe('#toString', function() {
+ *
+ *     it('turns 33 into the string "1, 1, 1, 1;"', function() {
+ *       const mrn = mr.convert(33);
+ *       const str = mrn.toString();
+ *
+ *       expect(str).to.equal('1, 1, 1, 1;');
+ *     });
+ *   });
+ *
+ *   describe('#toUnicodeString', function() {
+ *
+ *     it('turns 33 into the string "1, 1, 1, 1;"', function() {
+ *       const mrn = mr.convert(33);
+ *       const str = mrn.toUnicodeString();
+ *
+ *       expect(str).to.equal('1ₒₒ1₄1₃1₂');
+ *     });
+ *   });
+ *
+ *   describe('#add', function() {
+ *
+ *     it('adds 10 and 23 and gets 33', function() {
+ *       const n1 = mr.convert(10);
+ *       const n2 = mr.convert(23);
+ *
+ *       const n3 = n1.add(n2);
+ *
+ *       expect(n3._array).to.eql([1,1,1,1]);
+ *     });
+ *
+ *   });
+ *
+ * });
+ */
